80362323009072cde0ee6ec909fe25162817c8e5
George Obafemi <netherlesspictures@gmail.com>
2022-06-18 01:24:45 +0100

    removed unnecessary lines/structs

--- Buffer.cs
+++ Buffer.cs
@@ -6,39 +6,6 @@ namespace Bolt
     {
         public const int MaxLength = 1200;
 
-        [StructLayout(LayoutKind.Explicit)]
-        internal struct UIntFloat
-        {
-            [FieldOffset(0)]
-            public float floatValue;
-
-            [FieldOffset(0)]
-            public uint intValue;
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        internal struct ULongDouble
-        {
-            [FieldOffset(0)]
-            public double doubleValue;
-
-            [FieldOffset(0)]
-            public ulong longValue;
-        }
-
-        [StructLayout(LayoutKind.Explicit)]
-        internal struct ULongLongDecimal
-        {
-            [FieldOffset(0)]
-            public ulong longValue1;
-
-            [FieldOffset(8)]
-            public ulong longValue2;
-
-            [FieldOffset(0)]
-            public decimal decimalValue;
-        }
-
         protected int m_position;
         protected Segment m_segement;
 
