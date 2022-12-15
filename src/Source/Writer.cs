2159b9d3b886f589d65a646765a4ac315d027cbb
ALECIO <alecio@kezero.com>
2022-12-15 18:13:13 +0000

    Rebuild Writer

--- src/Source/Writer.cs
+++ src/Source/Writer.cs
@@ -9,6 +9,8 @@ namespace ByteBolt
     // creates a byte array, but he frist four byes are for header information
     public class Writer : Buffer
     {
+        #region Builder
+
         public Writer()
         {
             this.m_segement = new Segment(new byte[Default]);
@@ -21,45 +23,6 @@ namespace ByteBolt
             this.m_position = 0;
         }
 
-        public Writer(Writer other)
-        {
-            this.m_segement = other.m_segement;
-            this.m_position = other.m_position;
-        }
-
-        public Writer(ref Reader other, int lenght)
-        {
-            this.m_segement = other.ReadSegment(lenght);
-            this.m_position = 0;
-        }
-
-        public byte[] ToArray()
-        {
-            byte[] data = new byte[m_position];
-            m_segement.CopyTo(data, 0, m_position);
-            return data;
-        }
-
-        public Segment ToSegment()
-        {
-            return new Segment(m_segement.Array, m_segement.Offset, m_position);
-            /*return new Segment(ToArray());*/
-        }
-
-        public void Write(byte value)
-        {
-            m_segement[m_position] = value;
-            m_position++;
-        }
-
-        public void Write(byte[] values)
-        {
-            if (values.Length <= 0) return;
-
-            Array.Copy(values, 0, m_segement.Array, m_segement.GetRelativePosition(m_position), values.Length);
-            m_position += values.Length;
-        }
-
         public void Write(Segment values)
         {
             if (values.Count <= 0) return;
@@ -68,71 +31,127 @@ namespace ByteBolt
             m_position += values.Count;
         }
 
-        /*public void Write(sbyte value)
+        #endregion
+
+        #region Info
+
+        public byte[] ToArray()
         {
-            Write(Convert.ToByte(value));
-        }*/
+            byte[] data = new byte[m_position];
+            m_segement.CopyTo(data, 0, m_position);
+            return data;
+        }
+        
+        public Segment ToSegment()
+        {
+            return new Segment(m_segement.Array, m_segement.Offset, m_position);
+        }
+
+        #endregion
+
+        #region Write
+
+        public void Write(byte value)
+        {
+            m_segement[m_position] = value;
+            m_position++;
+        }        
 
         public void Write(bool value)
         {
-            Write(value ? 1 : 0);
+            WriteLine(value ? 1 : 0);
         }
 
         public void Write(char value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(short value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(ushort value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(int value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(uint value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(long value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(ulong value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(float value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
         public void Write(double value)
         {
-            Write(BitConverter.GetBytes(value));
+            WriteLine(BitConverter.GetBytes(value));
         }
 
-        /*public void Write(decimal value)
+        public void Write(string value, Encode encode = Encode.UTF8)
         {
+            byte[] bytes;
 
-        }*/
+            switch (encode)
+            {                
+                case Encode.ASCII:
+                    bytes = Encoding.ASCII.GetBytes(value);
+                    break;
+                case Encode.UTF7:
+                    bytes = Encoding.UTF7.GetBytes(value);
+                    break;
+                case Encode.UTF8:
+                    bytes = Encoding.UTF8.GetBytes(value);
+                    break;
+                case Encode.UTF32:
+                    bytes = Encoding.UTF32.GetBytes(value);
+                    break;
+                case Encode.UNICODE:
+                    bytes = Encoding.Unicode.GetBytes(value);
+                    break;
+                default:
+                    bytes = new byte[0];
+                    break;
+            }
 
-        public void Write(string value)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(value);
-            Write((ushort)bytes.Length);
             Write(bytes);
         }
+
+        public void Write(byte[] values)
+        {
+            if (values == null || values.Length <= 0) return;
+
+            WriteLine((int) values.Length);
+            WriteLine(values);
+        }
+
+        private void WriteLine(byte[] values)
+        {
+            if (values == null || values.Length <= 0) return;
+
+            Array.Copy(values, 0, m_segement.Array, m_segement.GetRelativePosition(m_position), values.Length);
+            m_position += values.Length;
+        }
+
+        #endregion
     }
 }
